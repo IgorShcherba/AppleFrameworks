062df4476c915fac162863f1be2a431c13f3e02b
Igor Shcherba <igor.shcherba@callstack.com>
2024-07-20 20:15:46 +0200

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Igor Shcherba on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
